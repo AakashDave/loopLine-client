aba188f862d4c3b8adfc2271232b70e3ca42331d
aakashdave <aakash@closedigit.com>
2023-08-26 12:48:53 +0530

    frontend First Commit

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import React from "react";
+import { BrowserRouter, Navigate, Routes, Route } from "react-router-dom";
+import HomePage from "pages/homePage";
+import LoginPage from "pages/loginPage";
+import ProfilePage from "pages/profilePage";
+import { useMemo } from "react";
+import { UseSelector, useSelector } from "react-redux/es/hooks/useSelector";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { createTheme } from "@mui/material";
+import { themeSettings } from "theme";
+import Navbar from "pages/navbar";
+
+const App = () => {
+  const mode = useSelector((state) => state.mode);
+  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
+
+  return (
+    <div className=".app">
+      <BrowserRouter>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Routes>
+            <Route path="/" element={<LoginPage />} />
+            <Route path="/home" element={<HomePage />} />
+            <Route path="/profile/:userId" element={<ProfilePage />} />
+          </Routes>
+        </ThemeProvider>
+      </BrowserRouter>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/pages/homePage/index.jsx
@@ -0,0 +1,15 @@
+import { Box } from '@mui/material'
+import Navbar from 'pages/navbar'
+import React from 'react'
+
+const HomePage = () => {
+  return (
+    <Box>
+      <Navbar />
+      <h1>Home Page</h1>
+    </Box>
+
+  )
+}
+
+export default HomePage
